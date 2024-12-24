ed6a87390bd8e6d693c465c27114997a7b8c8686
unknown <forkan.saadaoui@gmail.com>
2024-12-24 09:01:25 +0100

    connect db

---
+++ server/database/db.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "postgres",
+  }
+);
+
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+
+export default db;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import router from './routes/index.js'
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+import morgan from "morgan";
+import cors from "cors";
+import db from "./database/db.js";
 
-dotenv.config()
+dotenv.config();
+const port = process.env.PORT || 5001;
 
-const port = process.env.PORT || 5001
+const app = express();
 
-const app = express()
+app.use(morgan("dev"));
+app.use(cors());
 
-app.use('/', router)
+app.use("/", router);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+db.sync().then(() => {
+  app.listen(port, () => {
+    console.log("express is running on port " + port);
+  });
+});

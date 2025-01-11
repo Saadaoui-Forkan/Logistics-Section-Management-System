d3ea4b4d00f38492588afc86744245d06f30ebf7
unknown <forkan.saadaoui@gmail.com>
2025-01-11 11:31:46 +0100

    verify if the employee has unique fields

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -1,3 +1,4 @@
+import { Op } from "sequelize";
 import models from "../models/index.js";
 import { addEmployeeSchema } from "../utils/validationSchema.js";
 
@@ -28,51 +29,65 @@ export const getEmployees = async(req, res) => {
  * @method  POST
  * @access  private (only Admin or HR Administrator)
 */
-export const addEmployee = async(req, res) => {
-    try {
-        const user = await models.User.findOne({ where: { id: req.user.id } })
-        if (!user) {
-            return res.status(404).json({ message: "User Not Found" })
-        }
-
-        const {
-          full_name,
-          unique_identifier,
-          phone_number,
-          email,
-          date_of_birth,
-          position,
-          department,
-          comments,
-        } = req.body;
-        const validation = addEmployeeSchema.safeParse(req.body)
-        if (!validation.success) {
-          return res
-            .status(400)
-            .json({
-              message: validation.error.issues.map(err => `${err.path}: ${err.message}`)
-            });
-        }
-        const newEmployee = await models.Employee.create({
-          full_name,
-          unique_identifier,
-          phone_number,
-          email,
-          date_of_birth,
-          position,
-          department,
-          comments,
-          UserId: user.id
-        });
-
-        res.status(201).json({
-            message: "New Employee Is Added Successfully",
-            newEmployee
-        })
-    } catch (err) {
-        res.status(500).json({
-            message: "Internal Server Error",
-            error: err.message
-        })
+export const addEmployee = async (req, res) => {
+  try {
+    const user = await models.User.findOne({ where: { id: req.user.id } });
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
     }
-}
+
+    const {
+      full_name,
+      unique_identifier,
+      phone_number,
+      email,
+      date_of_birth,
+      position,
+      department,
+      comments,
+    } = req.body;
+    const validation = addEmployeeSchema.safeParse(req.body);
+    if (!validation.success) {
+      return res.status(400).json({
+        message: validation.error.issues.map(
+          (err) => `${err.path}: ${err.message}`
+        ),
+      });
+    }
+
+    const employee = await models.Employee.findOne({
+      where: {
+        [Op.or]: [
+          { full_name },
+          { unique_identifier },
+          { email },
+          { phone_number },
+        ],
+      },
+    });
+    if (employee) {
+      return res.status(400).json({ message: "Employee Already Exist!" });
+    } else {
+      const newEmployee = await models.Employee.create({
+        full_name,
+        unique_identifier,
+        phone_number,
+        email,
+        date_of_birth,
+        position,
+        department,
+        comments,
+        UserId: user.id,
+      });
+      res.status(201).json({
+        message: "New Employee Is Added Successfully",
+        newEmployee,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
